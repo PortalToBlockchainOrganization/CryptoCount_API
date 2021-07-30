2de5983764aa30211643344ac7774b4df5d410b8
HenrikMoe <henrikmoe29@gmail.com>
2021-07-29 17:22:59 -0700

    Working api; add query doc. Functioning: auto analysis, auto realizing; endpoints

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
-const server = "127.0.0.1:27017";
-const database = "cryptocount";
+//const server = "127.0.0.1:27017";
+//const database = "cryptocount";
 
-// Replace password with real password (ask shaheen)
 const MONGOURI =
 	"mongodb+srv://admin:lelloliar9876@postax.a1vpe.mongodb.net/AnalysisDep?retryWrites=true&w=majority";
 
--- backend/main.js
+++ backend/main.js
@@ -13,11 +13,38 @@ InitiateMongoServer();
 // Initiate Express (this) Server
 var app = express();
 
+
+app.get("/", (req, res)=>{
+	console.log(req.headers)
+	console.log(req.url)
+	console.log(req.path)
+	console.log(req.query)
+	console.log(req.subdomains) //data packer
+	console.log(req.params)
+	console.log(req.hostname)
+	res.status(404).end();
+})
+
+app.use("/Analysis", require("./Routes/Analysis/Anal.js"));
+
+
+app.get("/Analysis", (req, res)=>{
+	console.log(req.url)
+	console.log(req.params)
+	console.log(req.query)
+	console.log(req.validator)
+	res.status(404).end();
+})
+
 // Static paths to be served like index.html and all client side js
 app.use(express.static(path.join(__dirname, "public")));
 
+
+
+
 app.use(function (req, res, next) {
 	console.log("Handling " + req.path + "/" + req.method);
+	//
 	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
 	res.header("Access-Control-Allow-Credentials", true);
 	res.header("Access-Control-Allow-Headers", "Content-Type, Location");
@@ -26,20 +53,17 @@ app.use(function (req, res, next) {
 	next();
 });
 
+
 // No further processing needed for options calls.
+/*
 app.options("/*", function (req, res) {
 	res.status(200).end();
 });
+*/
 
 // Static path to index.html and all clientside js
 // Parse all request bodies using JSON
 app.use(bodyParser.json());
-
-// No messing w/db ids
-app.use(function (req, res, next) {
-	delete req.body.id;
-	next();
-});
 
 // Attach cookies to req as req.cookies.<cookieName>
 //app.use(cookieParser());
@@ -69,7 +93,7 @@ app.use(function (req, res, next) {
 // Load all subroutes
 //app.use("/Prss", require("./Routes/Account/Prss.js"));
 //app.use("/Ssns", require("./Routes/Account/Ssns.js"));
-app.use("/Anal", require("./Routes/Analysis/Anal.js"));
+//app.use("/Analysis", require("./Routes/Analysis/Anal.js"));
 
 // Anchor handler for general 404 cases.
 app.use(function (req, res) {
@@ -88,4 +112,6 @@ var portnum = args[args.indexOf("-p") + 1];
 
 app.listen(portnum, function () {
 	console.log("App Listening on port " + portnum);
+	console.log(app.status)
+
 });
